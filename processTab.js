39d7486de014252c7a6c77cb7ed7ae9d25d820b1
Cole Willsea <coleww@gmail.com>
2016-04-16 08:14:42 -0700

    cool, nice

--- processTab.js
+++ processTab.js
@@ -1,32 +1,39 @@
-module.exports = function (tabData) {
+function getSections (tabData) {
   // gosh, this is probably gonna be a total mess, huh?
   var lines = tabData.split(/\n/)
   var sections = []
-
-
-
+  var current = []
+  lines.forEach(function (line) {
+    // this regex is...probably good enough
+    if (line.match(/\w+-+\w+-+\w+-+/)) {
+      current.push(line)
+      if (current.length == 6) {
+        sections.push(current)
+        current = []
+      }
+    } else {
+      if (current.length == 4) sections.push(current) // should catch bass tabs i guess?
+      current = []
+    }
+  })
   return sections
-
-  // find sections of 4 or 6 lines in a row filled with '-'
-  // grab them lines, process them by the index in a row.
-  // for stuff like 3b or 12, just pop till u hit a `-` on all rows.
-  // if it breaks....whatever!
-  // map over the data, replace numbers with notes (just assume eadgbe for now, try to detect later)
-  //
 }
 
+var STRING_NOTES = ['e', 'a', 'd', 'g', 'b', 'e']
+var ALL_NOTES = ['e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'c', 'c#', 'd', 'd#']
+ALL_NOTES = ALL_NOTES.concat(ALL_NOTES).concat(ALL_NOTES) // if yr guitar has more than 30 frets i dont want yr song anyways
+function replaceNotes (section) {
+  return section.reverse().map(function (line, i) {
+    var root = STRING_NOTES[i]
+    var rootIndex = ALL_NOTES.indexOf(root)
+    var notes = line.replace(/[^\d-]/g, '').split('-')
+    return notes.map(function (note) {
+      return note.length ? ALL_NOTES[rootIndex + ~~note] : note
+    })
+  }).reverse()
+}
 
-// e|----------
-// B|----------
-// G|----------
-// D|--3--3--3-
-// A|--3--3--3-
-// E|--1--1--1-
-
-var sickPowerChords = [
-  ['', '', '', '', '', '', '', '', '', ''],
-  ['', '', '', '', '', '', '', '', '', ''],
-  ['', '', '', '', '', '', '', '', '', ''],
-  ['', '', 'f', '', '', 'f', '', '', 'f', ''],
-  ['', '', 'c', '', '', 'c', '', '', 'c', ''],
-  ['', '', 'f', '', '', 'f', '', '', 'f', '']]
+module.exports = {
+  getSections: getSections,
+  replaceNotes: replaceNotes
+}
