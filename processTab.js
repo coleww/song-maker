4bb98ed1246940c974c6292281edc3c24cc80326
Cole Willsea <coleww@gmail.com>
2016-04-16 10:50:17 -0700

    can get keys now tooooo

--- processTab.js
+++ processTab.js
@@ -23,62 +23,53 @@ var MIDI_NOTES = [40, 45, 50, 55, 59, 64] // maybe easier to keep this all in mi
 function replaceNotes (section) {
   return JSON.parse(JSON.stringify(section)).reverse().map(function (line, i) {
     var root = MIDI_NOTES[i]
-    var notes = line.replace(/[^\d-]/g, '').split('-')
+    var notes = line.replace(/[^\d-]/g, '').split('')
     return notes.map(function (note) {
-      return note.length ? root + ~~note : note
+      return note !== '-' ? root + ~~note : ''
     })
   }).reverse()
 }
 
+var midinote = require('midi-note')
+var uniq = require('lodash.uniq')
+var flatten = require('lodash.flatten')
+var key = require('music-key')
 
-function convertNotesToIndices (notes, divisor, key) {
-  // converts guitar strings worth of notes into indexes and stuff
-  return notes.map(function (row) {
-    return splitUp(row, divisor).map(function (part) {
-      return part.filter(function (n) {return n.length}).map(function (note) {
-
-      })
+function getKey (section) {
+  var notes = section.map(function (row) {
+    return row.filter(function (e) {return e}).map(function (note) {
+      return midinote(note).replace(/\d+$/g, '')
     })
   })
+  var accidentals = uniq(flatten(notes)).map(function (note) {
+    return note.replace(/^\w/, '')
+  }).filter(function (e) {return e})
+  return key(accidentals.join(''))
 }
 
+// function convertNotesToIndices (notes, beats, key) {
+//   // converts guitar strings worth of notes into indexes and stuff
+// var divisor = notes[0].length / beats
+//   return notes.map(function (row) {
+//     return chunk(row, divisor).map(function (part) {
+//       return part.filter(function (n) {return n.length}).map(function (note) {
 
-function convertNotesToMidi (notes, divisor, key) {
-  // eh, wouldn't be toooooo hard to do this....
-}
+//       })
+//     })
+//   })
+// }
+
+
+// function convertNotesToMidi (notes, divisor, key) {
+//   // eh, wouldn't be toooooo hard to do this....
+// }
 
 module.exports = {
   getSections: getSections,
   replaceNotes: replaceNotes,
-  convertNotesToIndices: convertNotesToIndices,
-  convertNotesToMidi: convertNotesToMidi
+  getKey: getKey
+  // convertNotesToIndices: convertNotesToIndices,
+  // convertNotesToMidi: convertNotesToMidi
 }
 
 
-
-// http://stackoverflow.com/a/8188682
-function splitUp(arr, n) {
-    var rest = arr.length % n, // how much to divide
-        restUsed = rest, // to keep track of the division over the elements
-        partLength = Math.floor(arr.length / n),
-        result = [];
-
-    for(var i = 0; i < arr.length; i += partLength) {
-        var end = partLength + i,
-            add = false;
-
-        if(rest !== 0 && restUsed) { // should add one element for the division
-            end++;
-            restUsed--; // we've used one division element now
-            add = true;
-        }
-
-        result.push(arr.slice(i, end)); // part of the array
-
-        if(add) {
-            i++; // also increment i in the case we added an extra element for division
-        }
-    }
-
-    return result;
-}
